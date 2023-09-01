0cc01a40a0c41d582fff9952a4845fba0e3ea34d
ajdin <ajdinhusetovic@outlook.com>
2023-09-01 20:25:21 +0200

    finish projects section

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
 import './projects.scss'
+import gameTagPicture from '../../assets/game-tag.png'
+import easyCrochetPicture from '../../assets/easy-crochet.png'
 
 export const Projects = () => {
   return (
-    <div id='projects'>Projects</div>
+    <article id='projects' className='projects-wrapper'>
+      <section className='projects-title'>
+        <h1>Projects</h1>
+      </section>
+      <article className='projects-cards-wrapper'>
+        <section className='projects-card'>
+          <section className='img-wrapper'>
+            <img src={gameTagPicture} />
+          </section>
+          <section className='project-content-wrapper'>
+            <p>Game Tag</p>
+            <button>View website</button>
+          </section>
+        </section>
+        <section className='projects-card'>
+          <section className='img-wrapper'>
+            <img src={easyCrochetPicture} />
+          </section>
+          <section className='project-content-wrapper'>
+            <p>Easy Crochet</p>
+            <button>View website</button>
+          </section>
+        </section>
+      </article>
+    </article>
   )
 }
