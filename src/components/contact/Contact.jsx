a37ae2af0ac06334c3c3589103a124f360026bf8
ajdin <ajdinhusetovic@outlook.com>
2023-09-03 22:18:58 +0200

    finish contact section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { AiFillGithub, AiFillLinkedin, AiOutlineMail, AiOutlineTwitter } from 'react-icons/ai'
+import './contact.scss'
 
 export const Contact = () => {
   return (
-    <div id='contact'>Contact</div>
+    <article id='contact' className='contact-wrapper'>
+      <section className='contact-title-wrapper'>
+        <h1>Contact</h1>
+      </section>
+      <section className='links-wrapper'>
+        <a href="https://github.com/ajdinhusetovic"><AiFillGithub /></a>
+        <a href="https://www.linkedin.com/in/ajdinhusetovic/"><AiFillLinkedin /></a>
+        <a href="mailto:ahusetovic1@gmail.com"><AiOutlineMail /></a>
+        <a href="https://twitter.com/ajdin_husetovic"><AiOutlineTwitter /></a>
+      </section>
+    </article>
   )
 }
