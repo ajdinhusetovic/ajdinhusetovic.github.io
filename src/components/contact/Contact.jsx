b13190e7d132fd96435cf50a430c994f963b0a39
ajdin <ajdinhusetovic@outlook.com>
2023-09-05 16:06:50 +0200

    Add framer motion animation to contact links

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { AiFillGithub, AiFillLinkedin, AiOutlineMail, AiOutlineTwitter } from 'react-icons/ai'
+import { motion } from 'framer-motion'
 import './contact.scss'
 
 export const Contact = () => {
@@ -9,10 +10,10 @@ export const Contact = () => {
         <h1>Contact</h1>
       </section>
       <section className='links-wrapper'>
-        <a href="https://github.com/ajdinhusetovic"><AiFillGithub /></a>
-        <a href="https://www.linkedin.com/in/ajdinhusetovic/"><AiFillLinkedin /></a>
-        <a href="mailto:ahusetovic1@gmail.com"><AiOutlineMail /></a>
-        <a href="https://twitter.com/ajdin_husetovic"><AiOutlineTwitter /></a>
+        <motion.a whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }} href="https://github.com/ajdinhusetovic"><AiFillGithub /></motion.a>
+        <motion.a whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }} href="https://www.linkedin.com/in/ajdinhusetovic/"><AiFillLinkedin /></motion.a>
+        <motion.a whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }} href="mailto:ahusetovic1@gmail.com"><AiOutlineMail /></motion.a>
+        <motion.a whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }} href="https://twitter.com/ajdin_husetovic"><AiOutlineTwitter /></motion.a>
       </section>
     </article>
   )
