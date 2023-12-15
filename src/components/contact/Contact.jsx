2c536c942e167f8e997d71db75ee5f2c66398ef7
Ajdin Husetovic <ahusetovic1@gmail.com>
2023-12-15 13:40:57 +0100

    update sections

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,20 +1,51 @@
-import React from 'react'
-import { AiFillGithub, AiFillLinkedin, AiOutlineMail, AiOutlineTwitter } from 'react-icons/ai'
-import { motion } from 'framer-motion'
-import './contact.scss'
+import {
+  AiFillGithub,
+  AiFillLinkedin,
+  AiOutlineMail,
+  AiOutlineTwitter,
+} from "react-icons/ai";
+import { motion } from "framer-motion";
+import "./contact.scss";
 
 export const Contact = () => {
   return (
-    <article id='contact' className='contact-wrapper'>
-      <section className='contact-title-wrapper'>
+    <article id="contact" className="contact-wrapper">
+      <section className="contact-title-wrapper">
         <h1>Contact</h1>
       </section>
-      <section className='links-wrapper'>
-        <motion.a whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }} href="https://github.com/ajdinhusetovic"><AiFillGithub /></motion.a>
-        <motion.a whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }} href="https://www.linkedin.com/in/ajdinhusetovic/"><AiFillLinkedin /></motion.a>
-        <motion.a whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }} href="mailto:ahusetovic1@gmail.com"><AiOutlineMail /></motion.a>
-        <motion.a whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }} href="https://twitter.com/ajdin_husetovic"><AiOutlineTwitter /></motion.a>
+      <section className="links-wrapper">
+        <motion.a
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.95 }}
+          href="https://github.com/ajdinhusetovic"
+          target="_blank"
+        >
+          <AiFillGithub />
+        </motion.a>
+        <motion.a
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.95 }}
+          href="https://www.linkedin.com/in/ajdinhusetovic/"
+          target="_blank"
+        >
+          <AiFillLinkedin />
+        </motion.a>
+        <motion.a
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.95 }}
+          href="mailto:ahusetovic1@gmail.com"
+        >
+          <AiOutlineMail />
+        </motion.a>
+        <motion.a
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.95 }}
+          href="https://twitter.com/ajdin_husetovic"
+          target="_blank"
+        >
+          <AiOutlineTwitter />
+        </motion.a>
       </section>
     </article>
-  )
-}
+  );
+};
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,37 +1,101 @@
-import React from 'react'
-import './projects.scss'
-import { motion } from 'framer-motion'
-import gameTagPicture from '../../assets/game-tag.png'
-import easyCrochetPicture from '../../assets/easy-crochet.png'
+import React from "react";
+import "./projects.scss";
+import { motion } from "framer-motion";
+import gameTagPicture from "../../assets/game-tag.png";
+import easyCrochetPicture from "../../assets/easy-crochet.png";
+import amhMoviesPicture from "../../assets/amhmovies.png";
 
 export const Projects = () => {
   return (
-    <article id='projects' className='projects-wrapper'>
-      <section className='projects-title'>
+    <article id="projects" className="projects-wrapper">
+      <section className="projects-title">
         <h1>Projects</h1>
       </section>
-      <article className='projects-cards-wrapper'>
-        <section className='projects-card'>
-          <section className='img-wrapper'>
+      <article className="projects-cards-wrapper">
+        <section className="projects-card">
+          <section className="img-wrapper">
             <img src={gameTagPicture} />
           </section>
-          <section className='project-content-wrapper'>
+          <section className="project-content-wrapper">
             <p>Game Tag</p>
-            <motion.a whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }} href="https://gametag.netlify.app/" target='blank'>View website</motion.a>
-            <motion.a whileHover={{scale: 1.1}} whileTap={{scale: 0.95}} href="https://github.com/ajdinhusetovic/game-tag" target='blank'>View GitHub</motion.a>
+            <p className="description">
+              Website where you can see the latest and best deals in the gaming
+              world
+            </p>
+            <motion.a
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.95 }}
+              href="https://gametag.netlify.app/"
+              target="blank"
+            >
+              View website
+            </motion.a>
+            <motion.a
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.95 }}
+              href="https://github.com/ajdinhusetovic/game-tag"
+              target="blank"
+            >
+              View GitHub
+            </motion.a>
           </section>
         </section>
-        <section className='projects-card'>
-          <section className='img-wrapper'>
+        <section className="projects-card">
+          <section className="img-wrapper">
             <img src={easyCrochetPicture} />
           </section>
-          <section className='project-content-wrapper'>
+          <section className="project-content-wrapper">
             <p>Easy Crochet</p>
-            <motion.a whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }} href="https://easycrochet.netlify.app/" target='blank'>View website</motion.a>
-            <motion.a whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }} href="https://github.com/ajdinhusetovic/easy-crochet" target='blank'>View GitHub</motion.a>
+            <p className="description">
+              Web app for croche enthusiasts, where you can upload your own
+              patterns
+            </p>
+            <motion.a
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.95 }}
+              href="https://easycrochet.netlify.app/"
+              target="blank"
+            >
+              View website
+            </motion.a>
+            <motion.a
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.95 }}
+              href="https://github.com/ajdinhusetovic/easy-crochet"
+              target="blank"
+            >
+              View GitHub
+            </motion.a>
+          </section>
+        </section>
+        <section className="projects-card">
+          <section className="img-wrapper">
+            <img src={amhMoviesPicture} />
+          </section>
+          <section className="project-content-wrapper">
+            <p>AMH Movies</p>
+            <p className="description">
+              Web app where that serves as your own movies to watch list
+            </p>
+            <motion.a
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.95 }}
+              href="https://amh-movies-ajdin-husetovics-projects.vercel.app/movies"
+              target="blank"
+            >
+              View website
+            </motion.a>
+            <motion.a
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.95 }}
+              href="https://github.com/ajdinhusetovic/amh-movies"
+              target="blank"
+            >
+              View GitHub
+            </motion.a>
           </section>
         </section>
       </article>
     </article>
-  )
-}
+  );
+};
