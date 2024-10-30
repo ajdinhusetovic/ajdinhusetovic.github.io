7cb6815f5590b248f5050d9a72ed7d753e33e1c7
Ajdin Husetovic <ahusetovic1@gmail.com>
2024-10-30 17:43:26 +0100

    Fix downloading resume

--- src/components/ResumeButton.tsx
+++ src/components/ResumeButton.tsx
@@ -4,10 +4,10 @@ const ResumeButton = () => {
   return (
     <motion.div
       className="bg-green-bg-three rounded-2xl mt-6 h-[60px] flex items-center justify-center xl:w-[200px] w-full cursor-pointer"
-      whileTap={{ scale: 1.1 }}
+      whileTap={{ scale: 0.9 }}
     >
       <a
-        href="src/assets/AjdinHusetovic.pdf"
+        href="/AjdinHusetovic.pdf"
         download="AjdinHusetovic.pdf"
         className="text-green-bg text-xl font-semibold w-full h-full flex items-center justify-center"
       >
